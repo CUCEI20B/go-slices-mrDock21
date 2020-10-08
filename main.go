20a7bcdf2b5c07e188a03e64340cccf51b2bc6bc
MrDock21 <dan.mejia@alumnos.udg.mx>
2020-10-08 12:40:05 -0500

    Implementada la suma de slice

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strconv"
+)
 
-func main()  {
-	fmt.Println("Hola Mundo!") // eliminar esta línea
+func main() {
+	var input string
+	var numbers []int
+	var sum int = 0
+
+	// Read from console
+	fmt.Scan(&input)
+
+	// Scan values from input string
+	N, _ := strconv.Atoi(input)
+
+	for i := 0; i < N; i++ {
+		fmt.Scan(&input)
+		number, _ := strconv.Atoi(input)
+		numbers = append(numbers, number)
+	}
+
+	for i := 0; i < len(numbers); i++ {
+		sum += numbers[i]
+	}
+	fmt.Println(sum)
 }
